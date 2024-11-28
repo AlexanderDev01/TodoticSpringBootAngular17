6e0c719676114e8f496d3abd69f7af583e7d451f
Luis Alexander <alexanderpd.dev@gmail.com>
2024-11-27 19:53:10 -0500

    Metodo post y delete

--- src/main/java/com/todotic/contactlistapi/controller/ContactController.java
+++ src/main/java/com/todotic/contactlistapi/controller/ContactController.java
@@ -7,10 +7,14 @@ import org.springframework.web.bind.annotation.RestController;
 import com.todotic.contactlistapi.entity.Contact;
 import com.todotic.contactlistapi.repository.ContactRepository;
 import java.time.LocalDateTime;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 
 
@@ -30,9 +34,32 @@ public class ContactController {
         return contactRepository.findById(id).orElse(null);
     }
     
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public Contact create(@RequestBody Contact contact) {
         contact.setCreatedAt(LocalDateTime.now());
         return contactRepository.save(contact);
+    }
+    
+    @PutMapping("{id}")
+    public Contact update(@PathVariable Integer id, 
+                          @RequestBody Contact form) {
+        Contact contactFromDb = contactRepository
+                                .findById(id)
+                                .orElse(null);
+        
+        contactFromDb.setName(form.getName());
+        contactFromDb.setEmail(form.getEmail());
+        
+        return contactRepository.save(contactFromDb);
+    }
+    
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable Integer id) {
+        Contact contactFromDb = contactRepository
+                                .findById(id)
+                                .orElse(null);
+        contactRepository.delete(contactFromDb);
     }
 }
